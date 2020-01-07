2d49ac45e7b0c3327b9939166f5297a7fda1d3b2
0kkun <shinji.okumoto.01@gmail.com>
2020-01-07 16:43:32 +0900

    install gem for Carrier Wave
    Gemのインストールと、messageコントローラーの編集

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,8 @@ class Message < ApplicationRecord
   #contentカラムとimageカラムのバリデーション設定
   #imageカラムが空の場合、contentカラムも空であれば保存しないという意味
   validates :content, presence: true, unless: :image?
+
+  #gemインストールして、uploader imageを生成したので、
+  #messageモデルとimage_uploaderをマウントする
+  mount_uploader :image, ImageUploader
 end
