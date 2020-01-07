ebde8409b43154ab6ea92f224bf5325e6584c0b4
0kkun <shinji.okumoto.01@gmail.com>
2020-01-07 12:58:26 +0900

    edit group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,16 @@
 class GroupsController < ApplicationController
 
+  #editアクションの定義をしなくても、edit.htmlは自動で表示できる
+  #editのルーティングと、それに対応するedit.htmlビューがあれば表示できる
+
+  #アクションが行われる前に、set_groupメソッドを実行する
+  #set_groupメソッドを実行するとgroupというインスタンス変数を生成する
+  #onlyは、editアクションとupdateアクションにのみ、有効とするという意味
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
-  
+
   def new #新規グループ作成画面を表示させる
     @group = Group.new #Groupモデルの新しいインスタンス変数を生成する
     @group.users << current_user #現在ログイン中のユーザーを、@groupに追加する
@@ -24,12 +32,24 @@ class GroupsController < ApplicationController
   def edit
   end
 
+  #グループの内容を更新する時のアクション
+  #
   def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
   
   private
+
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
