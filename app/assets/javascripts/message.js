e4159a2e78a1b6d21d0c08621352d5ed06794090
0kkun <shinji.okumoto.01@gmail.com>
2020-01-10 18:54:14 +0900

    Create jbuilder, modify messages.js and controller
    1. コントローラにjavascriptからのリクエストが来たらjbuilderへアクセスしてレスポンスを返すように記述。
    2. jbuilderを作成し、そこにコントローラからjsファイルに送るための値を記述
    3. jsファイルにコントローラから返ってきたレスポンスに対してhtmlを表示するように記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,80 @@
 $(function() {
 
+  // ------------------------関数定義----------------------------------
+  function buildHTML(message){
+    // メッセージに画像が含まれている場合の処理
+    if ( message.image ) {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+      
+    // メッセージに画像が含まれていない場合の処理
+    } else {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+
+  // ------------------------処理----------------------------------
+
   //form_forで作成したフォームには、#new_messageと言うidがデフォルトでついている
+  //フォーム内のsubmitが押されたら、イベントを実行する
   $('#new_message').on('submit', function(e) {
     e.preventDefault(); //デフォルトのイベントを止める
-    var message = $('.input-form__text').val();
-    console.log(message);
+
+    // var message = $('.input-form__text').val(); //チェック用
+    // console.log(message); //チェック用 中身確認用コンソール
+
+    var formData = new FormData(this); // form要素の中身を取得
+
+    // URLパスはフォーム要素のaction属性に格納されている
+    // attrでform要素のaction属性の値を指定して取得する
+    var url = $(this).attr('action') 
+
+    //messages controllerへcreateアクションをリクエストする
+    $.ajax({
+      url: url, ///groups/:id番号/messagesが入っている
+      type: "POST", //createアクションに対応するHTTPはPOST
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html); //htmlのmessagesクラスに、生成したhtmlを下に追加する。
+      $('form')[0].reset(); // フォームの中身をリセットする
+    })
+
   });
 });
