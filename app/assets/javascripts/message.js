8084110a77f71da4649011829c619e1e8288a244
0kkun <shinji.okumoto.01@gmail.com>
2020-01-12 23:51:46 +0900

    Add jbuilder crate, Edit js file
    1. メッセージの最も大きいid取得のため、jbuilderにmessage idを追加
    2. js fileに、コントローラから受け取ったid情報を使ってlast messageを0.7秒ごとに自動更新で表示できるように記述。
    また、グループのメッセージ一覧画面のみ処理を実行するように記述。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,5 @@
 $(function() {
-
+  
   // ------------------------関数定義----------------------------------
   function buildHTML(message){
     // メッセージに画像が含まれている場合の処理
@@ -84,4 +84,54 @@ $(function() {
     });
     return false;
   });
+
+// ------------------------チャット自動更新機能-----------------------------------
+
+  var reloadMessages = function() {
+    // jQueryのオブジェクトの指定方法 ":last" 
+    // .messageというクラスがつけられた全てのノードのうち一番最後のノードを取得
+    // つまり、カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    // console.log(last_message_id); //値取得確認用
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      // 自動でスクロールされるように、if文でmessageが0件意外の場合は実行するようにする
+      if (messages.length !== 0) {
+        // console.log('success'); //確認用
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        //自動スクロール機能を付加。animateを使う。
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("#new_message")[0].reset(); //フォームの中身を空にして再度送信できるようにする処理
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  // setInterval
+  // 第一引数に動かしたい関数名を、第二引数に動かす間隔をミリ秒単位で渡すことができる
+  // 7秒毎に処理を繰り返す
+  // グループのメッセージ一覧ページを表示している時だけ自動更新が行われるよう処理
+  // matchはメソッドを利用した文字列にその正規表現とマッチする部分があれば、
+  // それを含む配列を返り値とする
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+
 });
